ac509bbe120fa53a03cb44308f34ccc60b7d741f
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-06-08 15:21:24 -0700

    working code for type defs and createUser resolver

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,12 +7,30 @@ const typeDefs = gql`
   type Query {
     "Find the logged in user."
     me: User
+
+    """ nearbyRestrooms(location: [Int]): [Restroom]! """
+
+    singleRestroom(_id: ID!): Restroom
+
   }
 
   type Mutation {
     createUser(email: String!, password: String!, username: String!): Auth
     login(email: String!, password: String!): Auth
+    
+
+    saveRestroom(_Id: ID!, areaDescription: String!, location: [Int], changingStation: Boolean!, keyRequired: Boolean!, adaAccessible: Boolean!, reviews: [String]): User
+
+    createRestroom( areaDescription: String!, lat: Int, lon: Int, changingStation: Boolean!, keyRequired: Boolean!, adaAccessible: Boolean!): Restroom
+  
+    addReview(reviewText: String!, rating: Int!, username: String!): Restroom
   }
+
+
+type Location {
+  type: String
+  coordinates: [Int]
+}
 
   type Auth {
     token: String!
@@ -24,6 +42,26 @@ const typeDefs = gql`
     username: String!
     email: String!
     lastLogin: Date!
+    savedRestrooms: [Restroom]
+  }
+
+  type Restroom {
+    _id: ID!
+    areaDescription: String!
+    location: Location
+    changingStation: Boolean!
+    keyRequired: Boolean!
+    adaAccessible: Boolean!
+    reviews: [Review]
+    avgRating: Int!
+  }
+
+  type Review {
+    reviewText: String!
+    rating: Int!
+    createdAt: Date!
+    username: String!
+    userId: String!
   }
 `;
 
