5013a479f1a7c2baf42fba2e09d2fb00881c21c8
Christina <christinaa126@gmail.com>
2022-06-09 15:15:14 -0700

    fixed appbar links

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,38 @@
 import { useAuth } from "../util/auth";
+import blueToilets from "../images/blue_toilets.jpeg";
+import Paper from "@mui/material/Paper";
+
+const styles = {
+  formControl: {
+    display: "flex",
+    padding: "0.25em",
+  },
+  label: {
+    flex: "0 1 6em",
+    paddingRight: "0.25em",
+  },
+  paperContainer: {
+    backgroundImage: `url(${blueToilets})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    // height: 1300,
+  },
+};
 
 export default function Home() {
   const { isLoggedIn, user } = useAuth();
   return (
-    <div>
-      {/* TODO: display logged in user's username */}
-      <h1>Welcome {isLoggedIn ? user.username : "Guest"}!</h1>
-      <hr />
-      <h3 style={{ textAlignVertical: "center", textAlign: "center" }}>
-        <a href="./restroomsNearYou">Browse restrooms</a> near you and sign up
-        to help us add to our database!
-      </h3>
-      <p style={{ textAlignVertical: "center", textAlign: "center" }}>
-        must share location OR change location settings in browser
-      </p>
-    </div>
+    <Paper style={styles.paperContainer} sx={{ height: "100%" }}>
+      <div style={{ textAlignVertical: "center", textAlign: "center" }}>
+        {/* TODO: display logged in user's username */}
+        <h1>Welcome {isLoggedIn ? user.username : "Guest"}!</h1>
+        <hr />
+        <h3>
+          <a href="./restroomsNearYou">Browse restrooms</a> near you and sign up
+          to help us add to our database!
+        </h3>
+        <p>must share location OR change location settings in browser</p>
+      </div>
+    </Paper>
   );
 }
