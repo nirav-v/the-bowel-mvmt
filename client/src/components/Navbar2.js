bbab292aeb7455b1cdf06c7b3d2dd839c80e8e28
Elaine Deng <qd9069@gmail.com>
2022-06-09 19:59:43 -0700

    Modified navBar for log on left

--- client/src/components/Navbar2.js
+++ client/src/components/Navbar2.js
@@ -2,11 +2,11 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import BMLogo from "./logo";
 // import { NavLink } from "react-router-dom";
 import { useAuth } from "../util/auth";
+import Grid from "@mui/material/Grid";
 
 export default function Navbar2() {
   const { isLoggedIn, logout } = useAuth();
@@ -14,50 +14,57 @@ export default function Navbar2() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ bgcolor: "background.paper" }}>
         <Toolbar>
-          <Typography
-            variant="h4"
-            component="div"
-            sx={{ flexGrow: 1 }}
-          ></Typography>
-          <BMLogo sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          {/* <Grid container spacing={1} alignItems="center" direction="row" justifyContent="flex-end"> */}
+          <Grid
+            container
+            spacing={1}
+            alignItems="center"
+            direction="row"
+            justifyContent="space-between"
+          >
+            <Grid item sm={7} md={5}>
+              {/* <BMLogo sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
+              <BMLogo />
+            </Grid>
+            <Grid item sm={5} md={5}></Grid>
 
-          {isLoggedIn ? (
-            <>
-              <Button
-                sx={{ my: 2, color: "black", display: "block" }}
-                size="large"
-                href="/userpage"
-              >
-                Home
-              </Button>
-              <Button
-                sx={{ my: 2, color: "black", display: "block" }}
-                size="large"
-                href="/"
-                onClick={logout}
-              >
-                Logout
-              </Button>
-            </>
-          ) : (
-            <>
-              <Button
-                sx={{ my: 2, color: "black", display: "block" }}
-                size="large"
-                href="/signup"
-              >
-                Signup
-              </Button>
-              <Button
-                sx={{ my: 2, color: "black", display: "block" }}
-                size="large"
-                href="/login"
-              >
-                Login
-              </Button>
-            </>
-          )}
-
+            {isLoggedIn ? (
+              <>
+                <Button
+                  sx={{ my: 2, color: "black", display: "block" }}
+                  size="large"
+                  href="/userpage"
+                >
+                  Home
+                </Button>
+                <Button
+                  sx={{ my: 2, color: "black", display: "block" }}
+                  size="large"
+                  href="/"
+                  onClick={logout}
+                >
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  sx={{ my: 2, color: "black", display: "block" }}
+                  size="large"
+                  href="/signup"
+                >
+                  Signup
+                </Button>
+                <Button
+                  sx={{ my: 2, color: "black", display: "block" }}
+                  size="large"
+                  href="/login"
+                >
+                  Login
+                </Button>
+              </>
+            )}
+          </Grid>
         </Toolbar>
       </AppBar>
     </Box>
