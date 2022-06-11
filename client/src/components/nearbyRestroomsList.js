6a0feda5b131bddba4cf47d223237b2170ac9e45
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-06-10 17:08:51 -0700

    working nearby search code from johns help. Will need to delete database and rerun seeds to make it work

--- client/src/components/nearbyRestroomsList.js
+++ client/src/components/nearbyRestroomsList.js
@@ -1,73 +1,83 @@
 import { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useQuery, useLazyQuery } from "@apollo/client";
 import { Rating } from "@mui/material";
 import AccessibleIcon from "@mui/icons-material/Accessible";
 import BabyChangingStationIcon from "@mui/icons-material/BabyChangingStation";
 import KeyIcon from "@mui/icons-material/Key";
+import { Navigate } from "react-router-dom";
 
 import { NEARBY_RESTROOMS } from "../util/queries";
 
+const useCoords = () => {
+  const [error, setError] = useState(null);
+  const [coords, setCoords] = useState(null);
+  const [pending, setPending] = useState(true);
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const browserLon = position.coords.longitude;
+        const browserLat = position.coords.latitude;
+        const coords = { lat: browserLat, lon: browserLon };
+        setCoords(coords);
+        setPending(false);
+      },
+      (error) => {
+        setError(error);
+      }
+    );
+  } else {
+    setError(new Error("geolocation api unavailable"));
+  }
+  return { error, coords, pending };
+};
+
 export default function NearbyRestroomList() {
   // we declare out restrooms state variable with an initial state of false which will later be set to our array of nearby restrooms
-  const [restrooms, setRestroomState] = useState(false);
+  //const [restrooms, setRestroomState] = useState(false);
 
   // we have getUserLocation returning a promise that we can access the coordinates from
-  const getUserLocation = () =>
-    new Promise((resolve, reject) => {
-      if ("geolocation" in navigator) {
-        console.log("geolocation is available ");
-        navigator.geolocation.getCurrentPosition(
-          async (position) => {
-            const browserLon = await position.coords.longitude;
-            const browserLat = await position.coords.latitude;
-            const coords = { lat: browserLat, lon: browserLon };
-            resolve(coords);
-          },
-          (error) => {
-            console.log(error);
-            reject(error);
-          }
-        );
-      } else {
-        console.log("geolocation IS NOT available");
-        reject();
-      }
-    });
+  const userCoords = useCoords();
 
   // we only want to set our restrooms state after the NEARBY_RESTROOMS query has finished loading the data
-  const [getNearbyRestrooms, { loading, error, data }] = useLazyQuery(
-    NEARBY_RESTROOMS,
-    {
-      onCompleted: () => setRestroomState(data.nearbyRestrooms),
-    }
-  );
+  const [getNearbyRestrooms, { loading, error, data }] =
+    useLazyQuery(NEARBY_RESTROOMS);
 
   // useEffect prevents getUserLocation() from recursively running after each re-render of NearbyRestroomList component
   // we call the getNearbyRestrooms lazy query which returns an object with a nearbyRestrooms property containing our array of nearby restrooms
   useEffect(() => {
-    getUserLocation().then((res) => {
+    if (userCoords.coords) {
+      const { lat, lon } = userCoords.coords;
       getNearbyRestrooms({
-        variables: {
-          lat: res.lat,
-          lon: res.lon,
-        },
+        variables: { lat, lon },
       });
-    });
-  }, []);
+      console.count('fetching')
+    }
+  }, [userCoords.coords, getNearbyRestrooms]);
 
-  // show loading message until our array of restrooms is ready to render
-  if (!restrooms.length){
-    return <h2>LOADING NEARBY RESTROOMS...</h2>;
+
+  if (userCoords.pending) {
+    return <h2>your location is needed to find nearby restrooms</h2>;
   }
 
+  if (error || userCoords.error) {
+    alert("unexpected server error, redirecting to home page...");
+     return <Navigate to="/" />;
+  
+  }
+
+  // show loading message until our array of restrooms is ready to render
+  if (loading || !data) {
+    return <h2>LOADING NEARBY RESTROOMS...</h2>;
+  }
+const restrooms = data.nearbyRestrooms;
   return (
     <div>
       {restrooms &&
         restrooms.map((restroom) => (
           <div key={restroom._id}>
             <Link to={`/singleRestroom/${restroom._id}`}>
-            <h3>{restroom.areaDescription}</h3>
+              <h3>{restroom.areaDescription}</h3>
             </Link>
             <p>
               Rating:{" "}
